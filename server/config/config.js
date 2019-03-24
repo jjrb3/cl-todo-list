53d01df62194213b38b0eb5f33fda30a988f06d7
Jeremy Reyes B <jjrb6@hotmail.com>
2019-03-23 23:55:29 -0500

    [feat]: Remove google client.

--- server/config/config.js
+++ server/config/config.js
@@ -33,9 +33,3 @@ process.env.SEED =  process.env.SEED || 'this-is-the-seed';
 let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/todo' : 'mongodb://cafe-user:JJrb333@ds127190.mlab.com:27190/cafe';
 
 process.env.URLDB = urlDB;
-
-
-// ========================
-//  Google Client ID
-// ========================
-process.env.CLIENT_ID = process.env.CLIENT_ID || '293326936209-dl7pbmvpi6ugbg5u0mb951fakmh6hlna.apps.googleusercontent.com';
