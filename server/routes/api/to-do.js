5391e112cb16650f04b2bdb316a1a01dafc11bd4
Jeremy Reyes B <jjrb6@hotmail.com>
2019-03-24 20:46:17 -0500

    [feat]: update and delete To-Do.

--- server/routes/api/to-do.js
+++ server/routes/api/to-do.js
@@ -8,7 +8,6 @@ const { verifyToken } = require('../../middlewares/authentication');
 const app = express();
 
 
-
 app.get('/api/to-do', verifyToken, (req, res) => {
 
     Todo.find({})
@@ -64,5 +63,71 @@ app.post('/api/to-do', verifyToken, (req, res) => {
         });
     });
 });
+
+
+app.put('/api/to-do/:id', verifyToken, (req, res) => {
+
+    let id = req.params.id;
+    let body = req.body;
+
+
+    Todo.findByIdAndDelete(id, (err, dataDelete) => {
+
+        if (err) {
+            return res.status(400).json({
+                success: false,
+                err
+            });
+        }
+
+
+        var data = {
+            description: body.description,
+            status: body.status
+        };
+
+        if (body.user) {
+            data['user'] = body.user;
+        }
+
+        let todo = new Todo(data);
+
+        todo.save((err, todoDB) => {
+
+            if (err) {
+                return res.status(400).json({
+                    success: false,
+                    err
+                });
+            }
+
+            res.json({
+                success: true,
+                user: todoDB
+            });
+        });
+    });
+});
+
+
+app.delete('/api/to-do/:id', verifyToken, (req, res) => {
+
+    let id = req.params.id;
+
+    Todo.findByIdAndDelete(id, (err) => {
+
+        if (err) {
+            return res.status(400).json({
+                success: false,
+                err
+            });
+        }
+
+        res.json({
+            success: true
+        });
+    });
+});
+
 
 module.exports = app;
